3310c99396a97600682218aeca3a6a93d6fb0487
gkaveri <150512916+gkaveri@users.noreply.github.com>
2024-08-04 23:58:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SidePanel from './components/SidePanel';
+
+function App() {
+  return (
+    <div className="App">
+      <SidePanel />
+    </div>
+  );
+}
+
+export default App;
